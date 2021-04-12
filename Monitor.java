1054c5ca2786aa4d956c1766d9a523323f710591
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-04-12 19:28:06 +0400

    complete basic tasks

--- Monitor.java
+++ Monitor.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Class Monitor
  * To synchronize dining philosophers.
@@ -6,70 +8,122 @@
  */
 public class Monitor
 {
-	/*
-	 * ------------
-	 * Data members
-	 * ------------
-	 */
+    /*
+     * ------------
+     * Data members
+     * ------------
+     */
 
-	int chopsticks;
+    public enum Status {
+        THINKING, HUNGRY, EATING
+    }
 
-	enum Status {
-		thinking,
-		hungry,
-		eating
-	}
+    public boolean talk;
+    public  int philosophers;
+    public Status[] states;
+    public boolean[] chopsticks;
 
-	/**
-	 * Constructor
-	 */
-	public Monitor(int piNumberOfPhilosophers)
-	{
-		// TODO: set appropriate number of chopsticks based on the # of philosophers
+    /**
+     * Constructor
+     */
+    public Monitor(int piNumberOfPhilosophers)
+    {
+        talk = false;
+        philosophers = piNumberOfPhilosophers;
 
-	}
+        // if there is one philosopher, set chopsticks to two
+        chopsticks = new boolean[philosophers != 1 ? philosophers : philosophers + 1];
+        states = new Status[philosophers];
 
-	/*
-	 * -------------------------------
-	 * User-defined monitor procedures
-	 * -------------------------------
-	 */
+        Arrays.fill(states, Status.THINKING);
+        Arrays.fill(chopsticks, false);
+    }
 
-	/**
-	 * Grants request (returns) to eat when both chopsticks/forks are available.
-	 * Else forces the philosopher to wait()
-	 */
-	public synchronized void pickUp(final int piTID)
-	{
-		// ...
-	}
+    /*
+     * -------------------------------
+     * User-defined monitor procedures
+     * -------------------------------
+     */
+    public synchronized void test(final int piTID)
+    {
+		if(states[Math.abs((piTID-1)%philosophers)] != Status.EATING
+			&& states[Math.abs((piTID+1)%philosophers)] != Status.EATING
+			&& states[Math.abs((piTID)%philosophers)] == Status.HUNGRY)
+		{
+			states[piTID] = Status.EATING;
+			notifyAll();
+		}
+    }
+    /**
+     * Grants request (returns) to eat when both chopsticks/forks are available.
+     * Else forces the philosopher to wait()
+     */
+    public synchronized void pickUp(final int piTID)
+    {
+        int index = piTID - 1;
+        states[Math.abs(index%philosophers)] = Status.HUNGRY;
 
-	/**
-	 * When a given philosopher's done eating, they put the chopstiks/forks down
-	 * and let others know they are available.
-	 */
-	public synchronized void putDown(final int piTID)
-	{
-		// ...
-	}
+        test(index);
 
-	/**
-	 * Only one philopher at a time is allowed to philosophy
-	 * (while she is not eating).
-	 */
-	public synchronized void requestTalk()
-	{
-		// ...
-	}
+        if(states[Math.abs(index%philosophers)] != Status.EATING)
+        {
+            try
+            {
+                wait();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
 
-	/**
-	 * When one philosopher is done talking stuff, others
-	 * can feel free to start talking.
-	 */
-	public synchronized void endTalk()
-	{
-		// ...
-	}
+        states[Math.abs(index%philosophers)] = Status.EATING;
+        notifyAll();
+    }
+
+    /**
+     * When a given philosopher's done eating, they put the chopsticks/forks down
+     * and let others know they are available.
+     */
+    public synchronized void putDown(final int piTID)
+    {
+        int index = piTID - 1;
+        states[Math.abs(index%philosophers)] = Status.THINKING;
+        test(Math.abs((index-1)%philosophers));
+        test(Math.abs((index+1)%philosophers));
+    }
+
+    /**
+     * Only one philosopher at a time is allowed to philosophy
+     * (while she is not eating).
+     */
+    public synchronized void requestTalk()
+    {
+       while(talk)
+       {
+           try
+           {
+               wait();
+           }
+           catch (InterruptedException e)
+           {
+               e.printStackTrace();
+           }
+       }
+
+       talk = true;
+       notifyAll();
+    }
+
+    /**
+     * When one philosopher is done talking stuff, others
+     * can feel free to start talking.
+     */
+    public synchronized void endTalk()
+    {
+        talk = false;
+        notifyAll();
+    }
 }
 
 // EOF
